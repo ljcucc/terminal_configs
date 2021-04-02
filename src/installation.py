6af681bf53864515556acc54c45acb6b082c4fae
ljcucc <linjason920523@gmail.com>
2021-04-03 00:46:28 +0800

    move file into src folder

--- src/installation.py
+++ src/installation.py
@@ -46,3 +46,6 @@ def run():
 
         if prompt():
             return
+
+if __name__ == "__main__":
+    run()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 
 import installation
+import setup
 import terminal
 
 def init():
