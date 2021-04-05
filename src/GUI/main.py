132919cc4ededa3bf7deb8e25df99451401983f3
ljcucc <linjason920523@gmail.com>
2021-04-05 19:25:30 +0800

    create init GUI window class

---
+++ src/GUI/main.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import tkinter.font as font
+from tkinter.messagebox import askyesno, askquestion
+
+class Main(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("termianl-configs")
+        self.geometry('300x400')
+
+
+        btn1 = tk.Button(text="Install vimrc",
+            width = 18,
+            height = 2,
+            command=self.install_vimrc)
+        btn1.pack(expand=True)
+    
+    def install_vimrc(self):
+        answer = askyesno(title='Vimrc gonna install',
+            message='Your vimrc will be rewrite, do you want to continue?')
+
+# def createGUI():
+#     title = tk.Label(text="terminal-configs")
+#     title.config(width=20, height=3, font=("Arial", 25))
+#     title.pack()
+
+def run():
+    app = Main()
+    app.mainloop()
+
+if __name__ == "__main__":
+    print("please run from `make run`")
