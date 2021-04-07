019f38d10c63e8612fe29556451c8262b1ff1c57
ljcucc <linjason920523@gmail.com>
2021-04-07 17:00:27 +0800

    add OS check

--- src/terminal.py
+++ src/terminal.py
@@ -1,4 +1,18 @@
 import os
+import platform
 
 def clearScreen():
     os.system("clear")
+
+def checkOS():
+    print(os.name)
+    print(platform.system())
+    print(platform.release())
+    
+    release = platform.release()
+
+    if(release == "Darwin"):
+        return "macOS"
+
+if __name__ == "__main__":
+    checkOS()
